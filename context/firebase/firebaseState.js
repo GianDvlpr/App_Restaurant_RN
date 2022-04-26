5d5eeeee1890fc6d4c09f1006bdf8b1487e1759b
GianDvlpr <gian_franco_096@hotmail.com>
2022-04-26 14:29:27 -0500

    Configurando firebase y firestore

--- context/firebase/firebaseState.js
+++ context/firebase/firebaseState.js
@@ -3,6 +3,8 @@ import React, { useReducer } from 'react';
 import FirebaseReducer from './firebaseReducer'
 import FirebaseContext from './firebaseContext'
 
+import firebase from '../../firebase'
+
 const FirebaseState = props => {
 
     //Creando state inicial
@@ -16,7 +18,8 @@ const FirebaseState = props => {
     return (
         <FirebaseContext.Provider
             value={{
-                menu: state.menu
+                menu: state.menu,
+                firebase
             }}
         >
             {props.children}
---
+++ firebase/config.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_-fi5Yr3G4-aevRyVcwt6KCYhk8rO_eg",
+    authDomain: "restaurant-52d5e.firebaseapp.com",
+    databaseURL: "https://restaurant-52d5e-default-rtdb.firebaseio.com",
+    projectId: "restaurant-52d5e",
+    storageBucket: "restaurant-52d5e.appspot.com",
+    messagingSenderId: "823637559788",
+    appId: "1:823637559788:web:f9b3eab085dff23c8698c9",
+    measurementId: "G-HGGH07HRTV"
+};
+
+export default firebaseConfig
